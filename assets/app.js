19863fae8e8f5c4d09a1f0d5a354bcc928b3e0c0
sk-vision <karmakarsouvik101@gmail.com>
2025-03-21 19:04:00 +0530

    Create app.js

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+// let counter = 1;
+// // const numberOfImages = document.getElementsByClassName("imgCheck").length
+// // setInterval(function(){
+// //     document.getElementById('s' + counter).checked = true;
+// //     counter++;
+// //     if(counter > numberOfImages){
+// //         counter = 1;
+// //     }
+// // }, 1000);
+// //
+// // $('.imglabel').hover(function () {
+// //
+// // })
